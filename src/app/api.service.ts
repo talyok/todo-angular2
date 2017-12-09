47e0fb03d8b01b0dcd0c2179273565639f6be57e
Tal Herana <tal@1722itservices.com>
2017-12-09 20:03:13 +0800

    to do in angular 2 :(

---
+++ src/app/api.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+
+import { Http, Response } from '@angular/http';
+import { Todo } from './todo';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+const API_URL = environment.apiUrl;
+
+@Injectable()
+export class ApiService {
+
+  constructor(
+    private http: Http
+  ) {
+  }
+
+  private handleError (error: Response | any) {
+    console.error('ApiService::handleError', error);
+    return Observable.throw(error);
+  }
+
+  // API: GET /todos
+  public getAllTodos() {
+    return this.http
+    .get(API_URL + '/todos')
+    .map(response => {
+      const todos = response.json();
+      return todos.map((todo) => new Todo(todo));
+    })
+    .catch(this.handleError);
+  }
+
+  // API: POST /todos
+  public createTodo(todo: Todo) {
+    return this.http
+    .post(API_URL + '/todos', todo)
+    .map(response => {
+      return new Todo(response.json());
+    })
+    .catch(this.handleError);
+  }
+
+  // API: GET /todos/:id
+  public getTodoById(todoId: number) {
+    return this.http
+    .get(API_URL + '/todos/' + todoId)
+    .map(response => {
+      return new Todo(response.json());
+    })
+    .catch(this.handleError);
+  }
+
+  // API: PUT /todos/:id
+  public updateTodo(todo: Todo) {
+    console.log('line 59: ', todo)
+    return this.http
+    .put(API_URL + '/todos/' + todo.id, todo)
+    .map(response => {
+      return new Todo(response.json());
+    })
+    .catch(this.handleError);
+  }
+
+  // DELETE /todos/:id
+  public deleteTodoById(todoId: number) {
+    return this.http
+    .delete(API_URL + '/todos/' + todoId)
+    .map(response => null)
+    .catch(this.handleError);
+  }
+}
